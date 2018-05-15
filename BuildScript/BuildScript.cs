f949226c02e3a2841609dc1796b9bbf889abf2fc
markoz <mzorec@gmail.com>
2018-05-15 18:20:02 +0200

    deploy .net462 web api

--- BuildScript/BuildScript.cs
+++ BuildScript/BuildScript.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using FlubuCore.Context;
 using FlubuCore.Scripting;
+using Microsoft.Web.Administration;
 
 namespace BuildScript
 {
@@ -15,7 +16,14 @@ namespace BuildScript
         protected override void ConfigureTargets(ITaskContext session)
         {
             session.CreateTarget("Unzip.deploy.package")
-                .Do(UnzipDeployPackages);
+                .Do(UnzipDeployPackages)
+                .Do(Deploy);
+        }
+
+        protected void Deploy(ITaskContext context)
+        {
+            context.Tasks().RunProgramTask("C:\\DeploymentTests\\DeployPackages\\FlubuCore.WebApi-Net462\\build.exe")
+                .WorkingFolder("C:\\DeploymentTests\\DeployPackages\\FlubuCore.WebApi-Net462").Execute(context);
         }
 
         protected void UnzipDeployPackages(ITaskContext context)
@@ -28,13 +36,14 @@ namespace BuildScript
             }
 
             context.Tasks().CreateDirectoryTask("C:\\DeploymentTests", true).Execute(context);
+            context.Tasks().CreateDirectoryTask("C:\\DeploymentTests\\DeployPackages", true).Execute(context);
             foreach (var zip in zips)
             {
                 context.LogInfo($"Found zip file: {zip}.");
                 if (zip.StartsWith(@".\FlubuCore.WebApi-Net462"))
                 {
                     context.LogInfo($"Unziping '{zip}'.");
-                    context.Tasks().UnzipTask(zip, "C:\\DeploymentTests\\FlubuCore.WebApi-Net46").Execute(context);
+                    context.Tasks().UnzipTask(zip, "C:\\DeploymentTests\\DeployPackages\\FlubuCore.WebApi-Net462").Execute(context);
                 }
             }
         }
